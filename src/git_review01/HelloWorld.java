47b5f59fc3d0bc3560a88a09108f64dba7bcbb42
cansinbaycan <cansinbaycan@gmail.com>
2024-01-23 16:16:37 -0700

    hello world

--- src/git_review01/HelloWorld.java
+++ src/git_review01/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is hello world class!");
         System.out.println("Hello Cydeo");
+        System.out.println("hello");
     }
 }
